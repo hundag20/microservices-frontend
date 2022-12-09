53d2d3b4a7eeb1b6d52eb90eb9ce681154290052
Hunda Guluma <hundaguluma@gmail.com>
2022-12-09 09:43:22 +0300

    added month adjust buttons for YTD_dep'n_end_of_month AND YTD_book_value

--- src/components/styles/DataTableStyle.js
+++ src/components/styles/DataTableStyle.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
-const StyledDiv = styled.section``;
+const StyledDiv = styled.section`
+  .monthControlNav {
+    display: flex;
+    justify-content: center;
+  }
+  P {
+    font-size: 10px;
+  }
+`;
 
 export default StyledDiv;
--- src/components/ui/DataTable.js
+++ src/components/ui/DataTable.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import _DataTable from "react-data-table-component";
 import StyledDiv from "../styles/DataTableStyle";
 import { useSelector, useDispatch } from "react-redux";
+import { Button } from "@mui/material";
 
 let columns = [
   {
@@ -28,7 +29,6 @@ let data = [
     year: "1984",
   },
 ];
-
 const ExpandedComponent = ({ data }) => (
   <pre>{JSON.stringify(data, null, 2)}</pre>
 );
@@ -90,12 +90,32 @@ const Export = ({ onExport }) => (
 
 const DataTable = (props) => {
   const today = new Date();
-  const sb = useSelector((state) => state.sb.option);
-  const allData = props.fa_allData.fa_allData;
+  const [loading, setLoading] = useState(false);
 
+  const sb = useSelector((state) => state.sb.option);
+  const [allData, setAllData] = useState(props.fa_allData.fa_allData);
+
+  const [YTDBV_month, setYTDBV_month] = useState(today.getMonth() + 1);
+  const [YTDDE_month, setYTDDE_month] = useState(today.getMonth() + 1);
+
+  const YTDDE_monthUp = () => {
+    if (YTDDE_month < 12) setYTDDE_month(YTDDE_month + 1);
+  };
+  const YTDDE_monthDown = () => {
+    if (YTDDE_month > 1) setYTDDE_month(YTDDE_month - 1);
+  };
+  const YTDBV_monthUp = () => {
+    if (YTDBV_month < 12) setYTDBV_month(YTDBV_month + 1);
+  };
+  const YTDBV_monthDown = () => {
+    if (YTDBV_month > 1) setYTDBV_month(YTDBV_month - 1);
+  };
   columns = Object.entries(allData[0]);
+
+  const YTDBV_reg = new RegExp(/^YTDBooklaule@EndOfMonth/);
+  const YTDDE_reg = new RegExp(/^YTDDepEndOfMonth/);
+
   columns = columns.map((el) => {
-    var num = el[0].match(/\d+$/);
     if (el[0] === "Description") {
       return {
         name: "Description",
@@ -104,23 +124,36 @@ const DataTable = (props) => {
         sortable: true,
         format: (row) => `${row[el[0]].slice(0, 200)}`,
       };
-    } else if (el[0] === "DepreciationMethod" || el[0] === "AssetNLAccount")
+    } else if (el[0] === "DepreciationMethod" || el[0] === "AssetNLAccount") {
       return;
-    else if (
-      num &&
-      num[0] != 2022 &&
-      Number(num[0]) != Number(today.getMonth()) + 1
-    )
-      return;
-
+    } else if (YTDBV_reg.test(el[0])) {
+      const month = el[0].split("Month")[1];
+      if (month == YTDBV_month) {
+        return {
+          name: el[0],
+          selector: (row) => row[el[0]],
+          sortable: true,
+        };
+      } else return;
+    } else if (YTDDE_reg.test(el[0])) {
+      const month = el[0].split("Month")[1];
+      if (month == YTDDE_month) {
+        return {
+          name: el[0],
+          selector: (row) => row[el[0]],
+          sortable: true,
+        };
+      } else return;
+    }
     return {
       name: el[0],
       selector: (row) => row[el[0]],
       sortable: true,
     };
   });
+  //maybe month counter jst a var not a state
   columns = columns.filter((el) => el);
-
+  console.log("columns", columns);
   data = allData.map((el, i) => {
     if (el.Description) el.Description = el.Description.replaceAll(",", "");
 
@@ -128,8 +161,37 @@ const DataTable = (props) => {
     let { AssetNLAccount, DepreciationMethod, ...obj2 } = obj;
     let entries = Object.entries(obj);
     entries = entries.filter((el) => {
-      const num = el[0].match(/\d+$/);
-      if (num && num[0] != 2022 && num[0] != today.getMonth() + 1) return;
+      // if (YTDBV_reg.test(el[0])) {
+      //   if (YTDBV_month === "unset" && !YTDBV_month_lock) {
+      //     const month = el[0].split("Month")[1];
+      //     setYTDBV_month(month);
+      //     YTDBV_month_lock = true;
+      //     console.log("month for ", el[0], "is: ", month);
+
+      //     return {
+      //       name: el[0],
+      //       selector: (row) => row[el[0]],
+      //       sortable: true,
+      //       sortFunction: handleSort,
+      //     };
+      //   }
+      //   return;
+      // } else if (YTDDE_reg.test(el[0])) {
+      //   if (YTDDE_month === "unset" && !YTDDE_month_lock) {
+      //     const month = el[0].split("Month")[1];
+      //     setYTDDE_month(month);
+      //     YTDDE_month_lock = true;
+
+      //     console.log("month for ", el[0], "is: ", month);
+      //     return {
+      //       name: el[0],
+      //       selector: (row) => row[el[0]],
+      //       sortable: true,
+      //       sortFunction: handleSort,
+      //     };
+      //   }
+      //   return;
+      // }
       return el;
     });
     obj2 = Object.fromEntries(entries);
@@ -156,6 +218,32 @@ const DataTable = (props) => {
           marginRight: 0,
         }}
       >
+        <div className="monthControlNav">
+          <Button onClick={() => YTDDE_monthUp()}>
+            <p>
+              <i class="fas fa-plus-circle"></i>
+              {`YTD Dep'n End of month (now showing month: ${YTDDE_month})`}
+            </p>
+          </Button>
+          <Button onClick={() => YTDDE_monthDown()}>
+            <p>
+              <i class="fas fa-minus-circle"></i>
+              {`YTD Dep'n End of month (now showing month: ${YTDDE_month})`}
+            </p>
+          </Button>
+          <Button onClick={() => YTDBV_monthUp()}>
+            <p>
+              <i class="fas fa-plus-circle"></i>
+              {` YTD BookValue of month (now showing month: ${YTDBV_month})`}
+            </p>
+          </Button>
+          <Button onClick={() => YTDBV_monthDown()}>
+            <p>
+              <i class="fas fa-minus-circle"></i>
+              {`YTD BookValue of month (now showing month: ${YTDBV_month})`}
+            </p>
+          </Button>
+        </div>
         <_DataTable
           columns={columns}
           data={data}
@@ -165,6 +253,7 @@ const DataTable = (props) => {
           pagination
           fixedHeader
           fixedHeaderScrollHeight="450px"
+          progressPending={loading}
         />
       </div>
     </StyledDiv>
